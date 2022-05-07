f8e50447d454574e9d4ada35a4374225c4f1d08c
YaroslavKNU2024 <86433715+YaroslavKNU2024@users.noreply.github.com>
2022-05-07 13:33:32 +0200

    Add files via upload

---
+++ Controllers/ChartController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabaOne.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChartController : ControllerBase
+    {
+
+        private readonly DBFinalContext _context;
+        public ChartController(DBFinalContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("JsonDataVirusVariants")]
+        public JsonResult JsonDataVirusVariants()
+        {
+            var viruses = _context.Viruses.ToList();
+            List<object> variant = new List<object>();
+            variant.Add(new[] { "Вірус", "Кількість штамів" });
+            foreach(var v in viruses)
+            {
+                variant.Add(new object[] { v.VirusName, _context.Variants
+                    .Count(c => c.VirusId == v.Id)});
+            }
+            return new JsonResult(variant);
+        }
+
+
+    }
+}
